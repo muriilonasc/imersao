90e7fe085d5b988ed9d99cfaf485d75e83334215
muriilonasc <murilomns1611@gmail.com>
2024-09-05 16:16:33 -0300

    terceiro-commit

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("Olá, Mundo!");
+function pesquisar(){
+    let section = document.getElementById("resultados-pesquisa")
+console.log(section);
+
+let resultados = ""
+
+// para cada dado dentro da lista de dados
+for (let dado of dados){
+    resultados += `
+    <div class="item-resultado">
+            <h2>
+            ${dado.nome}
+            </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `
+}
+
+section.innerHTML = resultados
+
+}
+
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,12 @@
+let dados = [
+    {
+        nome: " Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma das maiores ginastas da história do Brasil. Com um talento inegável e uma força de vontade admirável, ela conquistou o coração dos brasileiros e se tornou um ícone do esporte nacional.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+    {
+        nome: "Marta Vieira da Silva",
+        descricao: "Considerada a maior jogadora de futebol feminino de todos os tempos, Marta conquistou o mundo com seu talento e habilidade. Com diversos títulos e recordes, ela é um ícone do esporte e inspiração para milhões de meninas e mulheres.",
+        link: "https://pt.wikipedia.org/wiki/Marta_Vieira_da_Silva"
+    }
+]
